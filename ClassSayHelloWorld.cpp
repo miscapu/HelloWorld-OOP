9666aad10ff72b32b70735dae0a7c43a564701c7
miscapu <mignan2010@outlook.com>
2019-05-27 21:58:57 -0300

    This class(SayHelloWorld) show: Hello World in display of the user

---
+++ ClassSayHelloWorld.cpp
@@ -0,0 +1,26 @@
+//including out library iostream with a directive include
+#include<iostream>
+
+//declaring out class SayHelloWorld
+class SayHelloWorld{
+//declare our function that will show on the screen Hello World
+public:
+    void printHelloWorld(){
+	//In the function printHelloWorld with cout instruction printed Hello World
+	std::cout << "Hello World" << std::endl;
+    } 
+};
+
+
+//declaring main function type int
+int main(){
+    //instantiate the class SayHelloWorld
+    //create the object print1 that contain the phrase
+    SayHelloWorld print1;
+    print1.printHelloWorld();     
+
+    //system pause so that the program stops one moment for show Hello World
+    system("pause");
+    //return 0 good practice
+    return 0;
+}
